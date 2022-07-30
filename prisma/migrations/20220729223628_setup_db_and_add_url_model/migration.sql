4e56fb9d231279a34af91973ed7b2214cc433c01
ZiiM <alextede8899@gmail.com>
2022-07-30 18:42:35 -0400

    Move to pscale

--- prisma/migrations/20220729223628_setup_db_and_add_url_model/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "url" (
-    "id" STRING NOT NULL,
-    "url" STRING NOT NULL,
-    "createdAt" TIMESTAMP(3) NOT NULL,
-    "updatedAt" TIMESTAMP(3) NOT NULL,
-
-    CONSTRAINT "url_pkey" PRIMARY KEY ("id")
-);
